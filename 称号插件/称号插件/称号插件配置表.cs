9907c34a454f8a19fd21e2953a68b0e0f4ed239c
qiweiff <106939073+qiweiff@users.noreply.github.com>
2022-10-31 07:42:18 +0800

    Add files via upload

--- 称号插件/称号插件/称号插件配置表.cs
+++ 称号插件/称号插件/称号插件配置表.cs
@@ -30,7 +30,7 @@ namespace 称号插件
         public string 说明1 = "称号设置中，前缀、名称、后缀设置为null则与玩家当前用户组的前后缀一致";
         public string 说明2 = "给称号指令:/称号 . 重载指令:/reload . 权限:“称号”";
         public string 说明3 = "改称号指令:/改称号  . 权限:“改称号”用于改自己的称号（需要先有一个称号）,可以给权限以允许普通玩家使用";
-        public string 说明4 = "一个汉字等于2字符,已设置的称号不受影响,用于防止普通玩家将自己的称号修改过长";
+        public string 说明4 = "已设置的称号不受最长字符影响,用于防止普通玩家将自己的称号修改过长";
         public bool 是否开启称号插件 { get; set; } = true;
         public int 称号最大字符数 { get; set; } = 13;
         public bool 是否允许管理绕过最大字符数检测 { get; set; } = true;
